1a8fdd4e92259718cbf55ff37620207050b2c4ef
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-08-22 02:47:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 355 ms, Memory: 88,132 KB -BaekjoonHub

---
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int d, w, k, minChemicals, film[][];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int tc = Integer.parseInt(st.nextToken());
+		for (int t = 1; t <= tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			d = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+
+			film = new int[d][w];
+			for (int i = 0; i < d; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < w; j++) {
+					film[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			minChemicals = Integer.MAX_VALUE;
+
+			if (impactTest(film)) {
+				minChemicals = 0;
+			} else {
+				subset(0, 0);
+			}
+
+			sb.append("#").append(t).append(" ").append(minChemicals).append('\n');
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	/**
+	 * 부분집합 구하기
+	 * 
+	 * @param index 인덱스
+	 * @param cnt   사용한 약품의 수
+	 */
+	public static void subset(int index, int cnt) {
+
+
+		// 사용 약품의 수가 최소 약품의 보다 많은 경우
+		if (cnt >= minChemicals)
+			return;
+
+		// 약품을 사용할 곳을 모두 정하고 최소 2개 이상 사용할 때
+		if (index == d) {
+			if (cnt < 1)
+				return;
+			if (!impactTest(film))
+				return;
+
+			minChemicals = cnt;
+
+			return;
+		}
+
+		// 약품을 사용할 depth의 배열을 저장
+		int[] originalArray = film[index].clone();
+
+		// 약품을 사용하지 않고
+		subset(index + 1, cnt);
+
+		// 약품 A를 사용
+		Arrays.fill(film[index], 0);
+		subset(index + 1, cnt + 1);
+
+		// 약품 B를 사용
+		Arrays.fill(film[index], 1);
+		subset(index + 1, cnt + 1);
+
+		// 원래대로 되돌리기
+		film[index] = originalArray;
+
+	}
+
+	/**
+	 * 충격 실험
+	 * 
+	 * @param matrix 테스트 할 배열
+	 * @return 통과 true, 실패 false
+	 */
+	public static boolean impactTest(int[][] matrix) {
+		if (k == 1)
+			return true;
+
+		// 위에서부터 아래로 확인
+		for (int j = 0; j < w; j++) {
+			boolean pass = false;
+			int cnt = 1;
+			for (int i = 1; i < d; i++) {
+				// 이전과 특성이 같으면
+				if (matrix[i][j] == matrix[i - 1][j]) {
+					cnt++;
+					// 통과시
+					if (cnt == k) {
+						pass = true;
+						break;
+					}
+					// 특성이 다르면 초기화
+				} else {
+					cnt = 1;
+				}
+			}
+			// 통과하지 못했다면
+			if (!pass)
+				return false;
+		}
+
+		// 통과했다면
+		return true;
+
+	}
+}

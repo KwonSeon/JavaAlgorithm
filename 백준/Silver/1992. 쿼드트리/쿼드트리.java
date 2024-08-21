3a73b2b9d26a9c5904e6171e2103a9f40824afb3
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-08-21 15:25:09 +0900

    [Silver I] Title: 쿼드트리, Time: 80 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ *
+ */
+public class Main {
+
+	static StringBuilder sb = new StringBuilder();
+	static int n, image[][];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		image = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < n; j++) {
+				if (line.charAt(j) == '0')
+					continue;
+				image[i][j]++;
+			}
+		}
+
+		
+		divideAndConquer(0, 0, n);
+		
+
+		System.out.println(sb.toString());
+	}
+
+	public static void divideAndConquer(int r, int c, int size) {
+
+		// 같은 경우
+		int compressedNum = compress(r, c, size);
+		if (compressedNum != -1) {
+			sb.append(compressedNum);
+			return;
+		}
+
+		// 같지 않은 경우
+		sb.append("(");
+		divideAndConquer(r, c, size / 2);
+		divideAndConquer(r, c + size / 2, size / 2);
+		divideAndConquer(r + size / 2, c, size / 2);
+		divideAndConquer(r + size / 2, c + size / 2, size / 2);
+		sb.append(")");
+	}
+
+	public static int compress(int r, int c, int size) {
+
+		int targetNum = image[r][c];
+		for (int i = r; i < r + size; i++) {
+			for (int j = c; j < c + size; j++) {
+				// 일치하지 않으면 -1 리턴
+				if (image[i][j] != targetNum)
+					return -1;
+			}
+		}
+
+		// 일치하면 압축
+		return targetNum;
+	}
+}

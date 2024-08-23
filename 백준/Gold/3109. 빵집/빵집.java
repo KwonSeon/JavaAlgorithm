69e33e957f87d67b944aaffc44395e676f2de179
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-08-23 09:56:22 +0900

    [Gold II] Title: 빵집, Time: 380 ms, Memory: 52120 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int R, C, map[][], cnt;
+	static int[] dr = { -1, 0, 1 };
+	static int[] dc = { 1, 1, 1 };
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new int[R][C];
+		for (int i = 0; i < R; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = makeMap(line.charAt(j));
+			}
+		}
+
+		visited = new boolean[R][C];
+		for (int i = 0; i < R; i++) {
+			dfs(i, 0);
+		}
+
+		for (int i = 0; i < R; i++) {
+			if (map[i][C - 1] == 1)
+				cnt++;
+		}
+
+
+
+		System.out.println(cnt);
+	}
+
+	public static boolean dfs(int r, int c) {
+
+		// 파이프 연결을 못할 시
+		if (map[r][c] == 1 || visited[r][c])
+			return false;
+
+		map[r][c] = 1;
+		visited[r][c] = true;
+		
+		// 마지막까지 파이프 연결을 성공할 때
+		if (c == C - 1) {
+			return true;
+		}
+
+		// 탐색
+		for (int i = 0; i < 3; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (nr < 0 || nr >= R || nc < 0 || nc >= C)
+				continue;
+
+			if (dfs(nr, nc)) {
+				return true;
+			}
+		}
+
+		map[r][c] = 0;
+		return false;
+	}
+
+	public static int makeMap(char d) {
+		if (d == '.') {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+}

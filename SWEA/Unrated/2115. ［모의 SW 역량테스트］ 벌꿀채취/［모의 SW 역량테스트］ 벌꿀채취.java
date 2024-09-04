56b5bec76abbc5dfa466c49429f8a02b9ca38ddf
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-09-04 11:44:09 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 168 ms, Memory: 32,392 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,9 +1,12 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * 메모리: 32,636KB, 시간: 165ms
+ */
 public class Solution {
 
-	static int N, M, C, beehive[][], maxEarning, maxEarning1, maxEarning2, hive1[][], hive2[][];
+	static int N, M, C, beehive[][], maxEarning, currentEarning, hive[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,16 +35,17 @@ public class Solution {
 				// 한 줄을 벗어나는 경우 다음으로
 				if (c1 > N - M)
 					continue;
-				
-				hive1 = new int[M][2];
+
+				hive = new int[M][2];
 				int idx = 0;
 				while (idx < M) {
-					hive1[idx] = new int[] { r1, c1 + idx };
+					hive[idx] = new int[] { r1, c1 + idx };
 					idx++;
 				}
-				maxEarning1 = 0;
-				subset1(0, 0, 0);
-				
+				currentEarning = 0;
+				subset(0, 0, 0);
+				int maxEarning1 = currentEarning;
+
 				for (int j = i + M; j < N * N; j++) {
 					int r2 = j / N;
 					int c2 = j % N;
@@ -49,14 +53,15 @@ public class Solution {
 					if (c2 > N - M)
 						continue;
 					// 채취할 꿀통의 인덱스 저장
-					hive2 = new int[M][2];
+					hive = new int[M][2];
 					idx = 0;
 					while (idx < M) {
-						hive2[idx] = new int[] { r2, c2 + idx };
+						hive[idx] = new int[] { r2, c2 + idx };
 						idx++;
 					}
-					maxEarning2 = 0;
-					subset2(0, 0, 0);
+					currentEarning = 0;
+					subset(0, 0, 0);
+					int maxEarning2 = currentEarning;
 
 					maxEarning = Math.max(maxEarning, maxEarning1 + maxEarning2);
 
@@ -69,42 +74,24 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	public static void subset1(int idx, int currentHoney, int currentEarning) {
+	public static void subset(int idx, int currentHoney, int current) {
 
 		// 채취할 수 있는 최대 양을 넘으면 return
 		if (currentHoney > C)
 			return;
 
 		// 최대 양 갱신
-        if (idx == M) {
-			maxEarning1 = Math.max(maxEarning1, currentEarning);
-		 	return;
-        }
 
-		// 채취
-		int honey = beehive[hive1[idx][0]][hive1[idx][1]];
-		subset1(idx + 1, currentHoney + honey, currentEarning + honey * honey);
-		// 미채취
-		subset1(idx + 1, currentHoney, currentEarning);
-	}
-
-	public static void subset2(int idx, int currentHoney, int currentEarning) {
-
-		// 채취할 수 있는 최대 양을 넘으면 return
-		if (currentHoney > C)
+		if (idx == M) {
+			currentEarning = Math.max(current, currentEarning);
 			return;
+		}
 
-		// 최대 양 갱신
-        if (idx == M){
-			maxEarning2 = Math.max(maxEarning2, currentEarning);
-		 	return;
-        }
-        
 		// 채취
-		int honey = beehive[hive2[idx][0]][hive2[idx][1]];
-		subset2(idx + 1, currentHoney + honey, currentEarning + honey * honey);
+		int honey = beehive[hive[idx][0]][hive[idx][1]];
+		subset(idx + 1, currentHoney + honey, current + honey * honey);
 		// 미채취
-		subset2(idx + 1, currentHoney, currentEarning);
+		subset(idx + 1, currentHoney, current);
 	}
 
 }

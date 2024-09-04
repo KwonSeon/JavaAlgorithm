1194934c00721c552e5bccdc98210b13c5b6d9d9
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-09-04 12:34:02 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1012 ms, Memory: 164156 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -9,9 +9,22 @@ public class Main {
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
+	static class Node {
+		int r;
+		int c;
+		int distance;
+		boolean state;
+
+		public Node(int r, int c, int distance, boolean state) {
+			this.r = r;
+			this.c = c;
+			this.distance = distance;
+			this.state = state;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
@@ -28,58 +41,47 @@ public class Main {
 	}
 
 	public static void bfs() {
-		Queue<int[]> q = new ArrayDeque<>();
-		visited = new boolean[n][m][2];
-		q.offer(new int[] { 0, 0, 0 });
+		Queue<Node> q = new ArrayDeque<>();
+		visited = new boolean[2][n][m];
+		q.offer(new Node(0, 0, 1, false));
 		visited[0][0][0] = true;
 
-		int depth = 1;
 		while (!q.isEmpty()) {
 
-			int size = q.size();
-			while (size-- > 0) {
-				int[] current = q.poll();
-				int r = current[0];
-				int c = current[1];
-				int state = current[2];
+			Node current = q.poll();
 
-				if (r == n - 1 && c == m - 1) {
-					System.out.println(depth);
-					return;
-				}
-
-				for (int dir = 0; dir < 4; dir++) {
-					int nr = r + dr[dir];
-					int nc = c + dc[dir];
-
-					if (nr < 0 || nr >= n || nc < 0 || nc >= m)
-						continue;
-					// 벽일 때
-					if (map[nr][nc] == 1) {
-						// 벽을 이미 한 번 부쉈을 때 통과
-						if (state == 1)
-							continue;
-						// 벽을 안 부쉈을 때
-						// 이미 방문했다면 통과
-						if (visited[nr][nc][1])
-							continue;
-						// 벽을 부수면서 방문
-						visited[nr][nc][1] = true;
-						q.offer(new int[] { nr, nc, 1 });
-
-						// 경로일 때
-					} else {
-						// 이미 방문했으면 통과
-						if (visited[nr][nc][state])
-							continue;
-						// 방문 처리
-						visited[nr][nc][state] = true;
-						q.offer(new int[] { nr, nc, state });
-					}
-
-				}
+			if (current.r == n - 1 && current.c == m - 1) {
+				System.out.println(current.distance);
+				return;
 			}
-			depth++;
+
+			for (int dir = 0; dir < 4; dir++) {
+				int nr = current.r + dr[dir];
+				int nc = current.c + dc[dir];
+
+				if (nr < 0 || nr >= n || nc < 0 || nc >= m)
+					continue;
+				// 이미 방문했으면 통과
+				if (visited[current.state ? 1 : 0][nr][nc])
+					continue;
+				// 벽일 때
+				if (map[nr][nc] == 1) {
+					// 벽을 이미 한 번 부쉈을 때 통과
+					if (current.state)
+						continue;
+					// 벽을 안 부쉈을 때
+					// 벽을 부수면서 방문
+					q.offer(new Node(nr, nc, current.distance + 1, true));
+
+					// 경로일 때
+				} else {
+					// 방문 처리
+					visited[current.state ? 1 : 0][nr][nc] = true;
+					q.offer(new Node(nr, nc, current.distance + 1, current.state));
+				}
+
+			}
+
 		}
 		// 경로가 없다면
 		System.out.println(-1);

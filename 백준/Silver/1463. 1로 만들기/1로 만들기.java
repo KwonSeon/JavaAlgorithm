99bd054dcaf128b003938a6f1e2ede7752a7d537
KWON SEON <122842139+KwonSeon@users.noreply.github.com>
2024-09-04 16:38:37 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15628 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -12,19 +12,14 @@ public class Main {
 		if (n > 2)
 			dp[3] = 1;
 		for (int i = 4; i <= n; i++) {
-			if (i % 2 == 0 && i % 3 == 0) {
-				dp[i] = Math.min(dp[i / 3], dp[i / 2]);
-				dp[i] = Math.min(dp[i], dp[i - 1]) + 1;
-				continue;
-			} else if (i % 3 == 0) {
-				dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
-				continue;
-			} else if (i % 2 == 0) {
-				dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
-				continue;
-			} else {
-				dp[i] = 1 + dp[i - 1];
-			}
+			dp[i] = dp[i - 1] + 1;
+            
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
 		}
 
 		System.out.println(dp[n]);
